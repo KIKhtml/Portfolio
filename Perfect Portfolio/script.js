14bb0451854f0fe221c61b9dcf1792d0c6983dab
KIKhtml <ks.tangsook@gmail.com>
2022-11-28 21:14:21 +0700

    #

--- Perfect Portfolio/script.js
+++ Perfect Portfolio/script.js
@@ -5,3 +5,27 @@ window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', window.scrollY > 0)
 });
 
+// renponsive navbar
+let menu = document.querySelector('#menu-icon')
+let navlist = document.querySelector('.navlist')
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navlist.classList.toggle('active')
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x')
+    navlist.classList.remove('active')
+};
+
+// animation
+const sr = ScrollReveal({
+    distance:'45px',
+    duration:2500,
+    reset:true
+})
+
+sr.reveal('.home-text', {delay:350, origin:'left'})
+sr.reveal('.home-img', {delay:350, origin:'right'})
+sr.reveal('.sub-service, .about, .portfolio, .service, .cta, .contact', {delay:200, origin:'bottom'})
